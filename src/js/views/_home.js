a040ea855238e93ed21825be9e1f2450ec0ed9e8
Dave Maynard <davemaynard24@gmail.com>
2019-08-04 20:52:52 -0400

    finished a static responsive version of the home template

--- src/js/views/_home.js
+++ src/js/views/_home.js
@@ -1,5 +1,20 @@
 const home = Vue.component('home', {
   template: `
-    <div>test!</div>
+  <div class="home-view">
+    <div class="home-view__bg" style="background-image: url('assets/img/focused-hd.jpg');"></div>
+    <div class="home-view__content">
+      <p class="h1 h1--large" aria-hidden="true">Design &amp; Code</p>
+      <div class="home-view__content-inner">
+        <p class="label label--upper">Home</p>
+      </div>
+      <div class="home-view__content-inner">
+        <h1>Design &amp; Code</h1>
+        <p>Aenean non orci id libero ornare efficitur id eu ex. Quisque quis ullamcorper nibh. Nam lobortis velit in sagittis imperdiet. Nam eu mi ut ante placerat efficitur. Nullam fringilla ante sit amet sem porttitor pulvinar.</p>
+      </div>
+    </div>
+    <aside class="home-view__aside">
+      <p class="h1 h1--xlarge" aria-hidden="true">Design &amp; Code</p>
+    </aside>
+  </div>
   `
 });
