db65a9044395baaf61e863f04659737491cc4f0b
Dave Maynard <davemaynard24@gmail.com>
2019-08-04 22:58:32 -0400

    started case study template, based off primary template design

--- src/js/views/_home.js
+++ src/js/views/_home.js
@@ -1,20 +1,30 @@
 const home = Vue.component('home', {
   template: `
-  <div class="home-view">
-    <div class="home-view__bg" style="background-image: url('assets/img/focused-hd.jpg');"></div>
-    <div class="home-view__content">
-      <p class="h1 h1--large" aria-hidden="true">Design &amp; Code</p>
-      <div class="home-view__content-inner">
-        <p class="label label--upper">Home</p>
+  <div class="primary-view">
+    <div class="primary-view__bg" :style="{ backgroundImage: 'url(' + image + ')' }"></div>
+    <div class="primary-view__content">
+      <p class="h1 h1--large" aria-hidden="true" v-html="title"></p>
+      <div class="primary-view__content-inner">
+        <p class="label label--upper">{{ label }}</p>
       </div>
-      <div class="home-view__content-inner">
-        <h1>Design &amp; Code</h1>
-        <p>Aenean non orci id libero ornare efficitur id eu ex. Quisque quis ullamcorper nibh. Nam lobortis velit in sagittis imperdiet. Nam eu mi ut ante placerat efficitur. Nullam fringilla ante sit amet sem porttitor pulvinar.</p>
+      <div class="primary-view__content-inner">
+        <h1 v-html="title"></h1>
+        <div v-html="description"></div>
       </div>
     </div>
-    <aside class="home-view__aside">
-      <p class="h1 h1--xlarge" aria-hidden="true">Design &amp; Code</p>
+    <aside class="primary-view__aside">
+      <p class="h1 h1--xlarge" aria-hidden="true" v-html="title"></p>
     </aside>
   </div>
-  `
+  `,
+  data: () => {
+    return {
+      image: `assets/img/focus.jpg`,
+      label: `HOME`,
+      title: `Design &amp; Code`,
+      description: `
+      <p>Aenean non orci id libero ornare efficitur id eu ex. Quisque quis ullamcorper nibh. Nam lobortis velit in sagittis imperdiet. Nam eu mi ut ante placerat efficitur. Nullam fringilla ante sit amet sem porttitor pulvinar.</p>
+      `
+    };
+  }
 });
