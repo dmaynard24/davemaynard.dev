d9b519db7d8b982f9a84244aeba695f79fcff3c6
dmaynard24 <davemaynard24@gmail.com>
2019-11-06 19:22:27 -0500

    added subtle mousemove event to primary view bg

--- src/js/views/_home.js
+++ src/js/views/_home.js
@@ -2,7 +2,12 @@ const home = Vue.component('home', {
   template: `
   <div class="primary-views">
     <div class="primary-view active">
-      <div class="primary-view__bg" :style="{ backgroundImage: 'url(' + image + ')' }"></div>
+      <div class="primary-view__bg">
+        <div class="primary-view__bg-inner" :style="{ 
+          backgroundImage: 'url(' + image + ')', 
+          transform: 'scale(1.05) translate(' + bgTranslateX + ', ' + bgTranslateY + ')' 
+        }"></div>
+      </div>
       <div class="primary-view__content">
         <p class="h1 h1--large" aria-hidden="true" v-html="title"></p>
         <div class="primary-view__content-inner">
@@ -27,7 +32,9 @@ const home = Vue.component('home', {
       title: `Design &amp; Code`,
       description: `
       <p>I'm an ambitious web developer with several years of experience creating functional, responsive websites and applications. I've worked closely with teams of designers and back-end developers, demonstrating the ability to communicate effectively with others in a fast-paced, agile environment.</p>
-      `
+      `,
+      bgTranslateX: '0%',
+      bgTranslateY: '0%'
     };
   },
   mounted: function() {
@@ -39,5 +46,23 @@ const home = Vue.component('home', {
     //   shared.pageIndex--;
     //   shared.setActivePrimaryView();
     // });
+
+    let _this = this;
+
+    function handleMouseOver(e) {
+      if (this.classList.contains('active')) {
+        let windowBounds = window.getWindowBounds(),
+          percentX = e.clientX / windowBounds.width,
+          percentY = e.clientY / windowBounds.height;
+
+        _this.bgTranslateX = (percentX * 2.25).toFixed(2) * -1 + '%';
+        _this.bgTranslateY = (percentY * 2.25).toFixed(2) * -1 + '%';
+      }
+    }
+
+    let primaryViews = document.querySelectorAll('.primary-view');
+    primaryViews.forEach(pv => {
+      pv.addEventListener('mousemove', handleMouseOver);
+    });
   }
 });
