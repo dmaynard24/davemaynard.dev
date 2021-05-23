4be7b61e3e4ace12a0b23e2d163728755f078686
Dave Maynard <dmayna@amazon.com>
2021-05-22 23:02:42 -0400

    initial commit for v4. using React, TS, and TailwindCSS

--- src/js/_mixins.js
+++
@@ -1,8 +0,0 @@
-const shared = {
-  pageIndex: 0,
-  setActivePrimaryView: function() {
-    let primaryViews = document.querySelector('.primary-views'),
-      transformString = `translateX(${shared.pageIndex * -100}vw)`;
-    primaryViews.style.transform = transformString;
-  }
-};
--- src/js/views/_app.js
+++
@@ -1,7 +0,0 @@
-const appView = Vue.component('app-view', {
-  template: `
-    <main id="app-view">
-      <router-view></router-view>
-    </main>
-  `
-});
